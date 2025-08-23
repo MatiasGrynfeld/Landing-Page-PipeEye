5b7c4906c57dd7e1d58626b1a3d49e29892c93ce
MatiasGrynfeld <48519558@est.ort.edu.ar>
2025-08-23 14:41:36 -0300

    caracteristicas terminado falta corregir el fondo

--- src/caracteristicas.jsx
+++ src/caracteristicas.jsx
@@ -2,12 +2,25 @@ import './css/caracteristicas.css';
 import CaracteristicasParte from './caracteristicas_parte.jsx';
 
 export default function Caracteristicas() {
-    return <section className='caracteristicas-wrapper'>
-        <div className='caracteristicas-content'>
-            <CaracteristicasParte img="/resources/caracteristicas/caracteristica 1.svg" texto="Analisis con IA para radiografias." />
-            <CaracteristicasParte img="/resources/caracteristicas/caracteristica 2.svg" texto="Entrenamiento de IA con casos reales." />
-            <CaracteristicasParte img="/resources/caracteristicas/caracteristica 3.svg" texto="Precision aproximada del 75%." />
-            <CaracteristicasParte img="/resources/caracteristicas/caracteristica 4.svg" texto="Mejoras en la IA todos los dias." />
-        </div>
-    </section>
+    return (
+        <section className='caracteristicas-wrapper'>
+            <div className='caracteristicas-container'>
+                <div className='caracteristicas-content'>
+                    <CaracteristicasParte img="/resources/caracteristicas/caracteristica 1.svg">
+                        Analisis con IA para radiografias.
+                    </CaracteristicasParte>
+                    <CaracteristicasParte img="/resources/caracteristicas/caracteristica 2.svg">
+                        Entrenamiento de IA con casos reales.
+                    </CaracteristicasParte>
+                    <CaracteristicasParte img="/resources/caracteristicas/caracteristica 3.svg">
+                        Precision aproximada del 75%.
+                    </CaracteristicasParte>
+                    <CaracteristicasParte img="/resources/caracteristicas/caracteristica 4.svg">
+                        Mejoras en la IA todos los dias.
+                    </CaracteristicasParte>
+                </div>
+                <button className='caracteristicas-button'>¿Como usar?</button>
+            </div>
+        </section>
+    );
 }
--- src/caracteristicas_parte.jsx
+++ src/caracteristicas_parte.jsx
@@ -1,11 +1,10 @@
-export default function CaracteristicasParte({ img, texto }) {
+export default function CaracteristicasParte({ img, children: texto }) {
     return (
         <div className="caracteristicas-parte">
             <img className="caracteristicas-parte-imagen" src={img} />
             <div className="caracteristicas-parte-texto-wrapper">
                 <p className="caracteristicas-parte-texto">{texto}</p>
             </div>
-            
         </div>
     );
 }
