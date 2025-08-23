39032f6a13d8a26ac62cd65b6b28225db2e17aa9
MatiasGrynfeld <48519558@est.ort.edu.ar>
2025-08-23 12:45:00 -0300

    reestructuro public

--- src/caracteristicas.jsx
+++ src/caracteristicas.jsx
@@ -4,10 +4,10 @@ import CaracteristicasParte from './caracteristicas_parte.jsx';
 export default function Caracteristicas() {
     return <section className='caracteristicas-wrapper'>
         <div className='caracteristicas-content'>
-            <CaracteristicasParte img="/resources/caracteristica 1.svg" texto="Analisis con IA para radiografias." />
-            <CaracteristicasParte img="/resources/caracteristica 2.svg" texto="Entrenamiento de IA con casos reales." />
-            <CaracteristicasParte img="/resources/caracteristica 3.svg" texto="Precision aproximada del 75%." />
-            <CaracteristicasParte img="/resources/caracteristica 4.svg" texto="Mejoras en la IA todos los dias." />
+            <CaracteristicasParte img="/resources/caracteristicas/caracteristica 1.svg" texto="Analisis con IA para radiografias." />
+            <CaracteristicasParte img="/resources/caracteristicas/caracteristica 2.svg" texto="Entrenamiento de IA con casos reales." />
+            <CaracteristicasParte img="/resources/caracteristicas/caracteristica 3.svg" texto="Precision aproximada del 75%." />
+            <CaracteristicasParte img="/resources/caracteristicas/caracteristica 4.svg" texto="Mejoras en la IA todos los dias." />
         </div>
     </section>
 }
