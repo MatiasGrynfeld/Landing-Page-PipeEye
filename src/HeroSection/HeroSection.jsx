58d640b6c0c03a32303c3abb0fcb536b1937be00
gabislot07 <gabislot07@gmail.com>
2025-09-14 14:39:24 -0300

    Me puse las pilas con la landing!!

--- src/HeroSection/HeroSection.jsx
+++ src/HeroSection/HeroSection.jsx
@@ -1,5 +1,22 @@
 import './HeroSection.css';
 
+
 export default function HeroSection() {
-    return <div></div>
+    return <div className='herosection'>
+        <div className='herosection-description-container'>
+            <p className='herosection-description'>IA que analiza automáticamente radiografías de tuberías para detectar, segmentar y clasificar posibles defectos.</p>
+        </div>
+        <div className='herosection-bottom-wrapper'>
+            <div className='herosection-bottom-left'>
+                <img src="/resources/herosection/buttonpng.png" className='herosection-bottom-left-image'/>
+                <button className='herosection-bottom-left-button'>
+                    Conocer más
+                </button>
+            </div>
+            <div className='herosection-bottom-right'>
+                <p className='herosection-bottom-right-text'>Analizá soldaduras de ductos con la IA a tu lado.</p>
+            </div>
+        </div>
+    </div>
 }
+
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,5 +1,15 @@
+
+
 import './Navbar.css';
 
+
 export default function Navbar() {
-    return <nav></nav>
+    return <nav className='navbar'>
+        <div className='navbar-logo-container'>
+            <img src="/resources/navbar/PipeEye.png" className='navbar-logo' />
+        </div>
+        <div className='navbar-button-container'>
+            <button className='navbar-button'>Ya soy cliente</button>
+        </div>
+    </nav>
 }
