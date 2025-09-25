591da89fd704c3f015f8bd08373ca262f566a617
GRYNFELD MATIAS <48519558@ort>
2025-09-25 09:07:42 -0300

    animacion carrusel

--- src/Asociados/Asociados.jsx
+++ src/Asociados/Asociados.jsx
@@ -1,6 +1,35 @@
 import './Asociados.css';
+import { useState, useEffect } from 'react';
 
 export default function Asociados() {
+    const empresas = [
+        "/resources/asociados/inspec group.png",
+        "/resources/asociados/end tech.png",
+        "/resources/asociados/enod.png",
+        "/resources/asociados/ndt solutions.png",
+        "/resources/asociados/ndt systems.png"
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [isTransitioning, setIsTransitioning] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsTransitioning(true);
+            setTimeout(() => {
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % empresas.length);
+                setIsTransitioning(false);
+            }, 600);
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [empresas.length]);
+
+    const getImageAtPosition = (position) => {
+        const index = (currentIndex + position + empresas.length) % empresas.length;
+        return empresas[index];
+    };
+
     return <div className='asociados'>
         <div className='asociados-title-wrapper'>
             <div className='asociados-main-title-container'>
@@ -24,12 +53,38 @@ export default function Asociados() {
                 </div>
             </div>
         </div>
-        <div className='asociados-empresas-wrapper'>
-            <img src="/resources/asociados/inspec group.png" className="asociado-empresa" id="empresa-extremo-izq" />
-            <img src="/resources/asociados/end tech.png" className="asociado-empresa" id="empresa-izq" />
-            <img src="/resources/asociados/enod.png" className="asociado-empresa" id='empresa-medio'/>
-            <img src="/resources/asociados/ndt solutions.png" className="asociado-empresa" id="empresa-der" />
-            <img src="/resources/asociados/ndt systems.png" className="asociado-empresa" id="empresa-extremo-der" />
+        <div className={`asociados-empresas-wrapper ${isTransitioning ? 'transitioning' : ''}`}>
+            <img 
+                key={`extremo-izq-${currentIndex}`}
+                src={getImageAtPosition(-2)} 
+                className="asociado-empresa carousel-image" 
+                id="empresa-extremo-izq"
+            />
+            <img 
+                key={`izq-${currentIndex}`}
+                src={getImageAtPosition(-1)} 
+                className="asociado-empresa carousel-image" 
+                id="empresa-izq"
+            />
+            <img 
+                key={`medio-${currentIndex}`}
+                src={getImageAtPosition(0)} 
+                className="asociado-empresa carousel-image" 
+                id='empresa-medio'
+                alt="Empresa asociada principal"
+            />
+            <img 
+                key={`der-${currentIndex}`}
+                src={getImageAtPosition(1)} 
+                className="asociado-empresa carousel-image" 
+                id="empresa-der"
+            />
+            <img 
+                key={`extremo-der-${currentIndex}`}
+                src={getImageAtPosition(2)} 
+                className="asociado-empresa carousel-image" 
+                id="empresa-extremo-der"
+            />
         </div>
     </div>
 }
